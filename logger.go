e99f94a92f72f41a6c9ae323fc1d9a5b004a160a
Nevroz Arslan <nevroz@veriyaz.com.tr>
2022-02-26 13:29:34 +0300

    add middleware function

--- logger.go
+++ logger.go
@@ -1,1 +1,30 @@
 package reqlogger
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"github.com/uptrace/bunrouter"
+)
+
+type middleware struct{}
+
+// NewMiddleware creates a middleware instance ...
+func NewMiddleware() bunrouter.MiddlewareFunc {
+	m := &middleware{}
+	return m.Middleware
+}
+
+// Middleware ...
+func (m *middleware) Middleware(next bunrouter.HandlerFunc) bunrouter.HandlerFunc {
+
+	return func(w http.ResponseWriter, req bunrouter.Request) error {
+		now := time.Now()
+		err := next(w, req)
+		dur := time.Since(now)
+
+		log.Info().Dur("latency", dur).Msg("")
+		return err
+	}
+}
--- logger_test.go
+++ logger_test.go
@@ -10,8 +10,28 @@ import (
 	"github.com/uptrace/bunrouter"
 )
 
+//Test the router. It serves as aboilerplate. Remove this soon.
 func TestRequestWithContext(t *testing.T) {
 	router := bunrouter.New()
+	router.GET("/user/:param", func(w http.ResponseWriter, req bunrouter.Request) error {
+		value1 := req.Param("param")
+		require.Equal(t, "hello", value1)
+
+		value2 := req.WithContext(context.TODO()).Param("param")
+		require.Equal(t, value1, value2)
+
+		return nil
+	})
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/user/hello", nil)
+	router.ServeHTTP(w, req)
+}
+
+func TestMiddleware(t *testing.T) {
+	router := bunrouter.New(
+		bunrouter.WithMiddleware(NewMiddleware()),
+	)
 	router.GET("/user/:param", func(w http.ResponseWriter, req bunrouter.Request) error {
 		value1 := req.Param("param")
 		require.Equal(t, "hello", value1)
